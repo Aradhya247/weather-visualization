fb01c936a654edd2008e5ca580a90859946e14ef
Hi, Aradhya Mishra here! <166850265+Aradhya247@users.noreply.github.com>
2025-07-24 19:20:54 +0530

    statistics.py

--- _statistics.py
+++ _statistics.py
@@ -1,28 +1,4 @@
 """Statistical transformations for visualization.
-
-This module is currently private, but is being written to eventually form part
-of the public API.
-
-The classes should behave roughly in the style of scikit-learn.
-
-- All data-independent parameters should be passed to the class constructor.
-- Each class should implement a default transformation that is exposed through
-  __call__. These are currently written for vector arguments, but I think
-  consuming a whole `plot_data` DataFrame and return it with transformed
-  variables would make more sense.
-- Some class have data-dependent preprocessing that should be cached and used
-  multiple times (think defining histogram bins off all data and then counting
-  observations within each bin multiple times per data subsets). These currently
-  have unique names, but it would be good to have a common name. Not quite
-  `fit`, but something similar.
-- Alternatively, the transform interface could take some information about grouping
-  variables and do a groupby internally.
-- Some classes should define alternate transforms that might make the most sense
-  with a different function. For example, KDE usually evaluates the distribution
-  on a regular grid, but it would be useful for it to transform at the actual
-  datapoints. Then again, this could be controlled by a parameter at  the time of
-  class instantiation.
-
 """
 from numbers import Number
 from statistics import NormalDist
@@ -51,26 +27,6 @@ class KDE:
         cumulative=False,
     ):
         """Initialize the estimator with its parameters.
-
-        Parameters
-        ----------
-        bw_method : string, scalar, or callable, optional
-            Method for determining the smoothing bandwidth to use; passed to
-            :class:`scipy.stats.gaussian_kde`.
-        bw_adjust : number, optional
-            Factor that multiplicatively scales the value chosen using
-            ``bw_method``. Increasing will make the curve smoother. See Notes.
-        gridsize : int, optional
-            Number of points on each dimension of the evaluation grid.
-        cut : number, optional
-            Factor, multiplied by the smoothing bandwidth, that determines how
-            far the evaluation grid extends past the extreme datapoints. When
-            set to 0, truncate the curve at the data limits.
-        clip : pair of numbers or None, or a pair of such pairs
-            Do not evaluate the density outside of these limits.
-        cumulative : bool, optional
-            If True, estimate a cumulative distribution function. Requires scipy.
-
         """
         if clip is None:
             clip = None, None
@@ -208,35 +164,7 @@ class Histogram:
         discrete=False,
         cumulative=False,
     ):
-        """Initialize the estimator with its parameters.
-
-        Parameters
-        ----------
-        stat : str
-            Aggregate statistic to compute in each bin.
-
-            - `count`: show the number of observations in each bin
-            - `frequency`: show the number of observations divided by the bin width
-            - `probability` or `proportion`: normalize such that bar heights sum to 1
-            - `percent`: normalize such that bar heights sum to 100
-            - `density`: normalize such that the total area of the histogram equals 1
-
-        bins : str, number, vector, or a pair of such values
-            Generic bin parameter that can be the name of a reference rule,
-            the number of bins, or the breaks of the bins.
-            Passed to :func:`numpy.histogram_bin_edges`.
-        binwidth : number or pair of numbers
-            Width of each bin, overrides ``bins`` but can be used with
-            ``binrange``.
-        binrange : pair of numbers or a pair of pairs
-            Lowest and highest value for bin edges; can be used either
-            with ``bins`` or ``binwidth``. Defaults to data extremes.
-        discrete : bool or pair of bools
-            If True, set ``binwidth`` and ``binrange`` such that bin
-            edges cover integer values in the dataset.
-        cumulative : bool
-            If True, return the cumulative statistic.
-
+        """Initialize the estimator with its parameters
         """
         stat_choices = [
             "count", "frequency", "density", "probability", "proportion", "percent",
@@ -404,13 +332,6 @@ class ECDF:
     def __init__(self, stat="proportion", complementary=False):
         """Initialize the class with its parameters
 
-        Parameters
-        ----------
-        stat : {{"proportion", "percent", "count"}}
-            Distribution statistic to compute.
-        complementary : bool
-            If True, use the complementary CDF (1 - CDF)
-
         """
         _check_argument("stat", ["count", "percent", "proportion"], stat)
         self.stat = stat
@@ -458,20 +379,7 @@ class EstimateAggregator:
 
     def __init__(self, estimator, errorbar=None, **boot_kws):
         """
-        Data aggregator that produces an estimate and error bar interval.
-
-        Parameters
-        ----------
-        estimator : callable or string
-            Function (or method name) that maps a vector to a scalar.
-        errorbar : string, (string, number) tuple, or callable
-            Name of errorbar method (either "ci", "pi", "se", or "sd"), or a tuple
-            with a method name and a level parameter, or a function that maps from a
-            vector to a (min, max) interval, or None to hide errorbar. See the
-            :doc:`errorbar tutorial </tutorial/error_bars>` for more information.
-        boot_kws
-            Additional keywords are passed to bootstrap when error_method is "ci".
-
+        
         """
         self.estimator = estimator
 
@@ -578,30 +486,8 @@ class WeightedAggregator:
 
 class LetterValues:
 
-    def __init__(self, k_depth, outlier_prop, trust_alpha):
-        """
-        Compute percentiles of a distribution using various tail stopping rules.
-
-        Parameters
-        ----------
-        k_depth: "tukey", "proportion", "trustworthy", or "full"
-            Stopping rule for choosing tail percentiled to show:
-
-            - tukey: Show a similar number of outliers as in a conventional boxplot.
-            - proportion: Show approximately `outlier_prop` outliers.
-            - trust_alpha: Use `trust_alpha` level for most extreme tail percentile.
-
-        outlier_prop: float
-            Parameter for `k_depth="proportion"` setting the expected outlier rate.
-        trust_alpha: float
-            Parameter for `k_depth="trustworthy"` setting the confidence threshold.
-
-        Notes
-        -----
-        Based on the proposal in this paper:
-        https://vita.had.co.nz/papers/letter-value-plot.pdf
-
-        """
+    def __init__(self, k_depth, outlier_prop, trust_alpha)
+  """
         k_options = ["tukey", "proportion", "trustworthy", "full"]
         if isinstance(k_depth, str):
             _check_argument("k_depth", k_options, k_depth)
